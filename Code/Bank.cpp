2efe97f7a5f7e7e1ea1a8e6ae091ab73f154ef1b
kirtimishraa <81264089+kirtimishraa@users.noreply.github.com>
2025-09-08 15:15:05 +0530

    update

--- Code/Bank.cpp
+++ Code/Bank.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 class Bank{
-    float Amount, Money;            //Amount is total amount, Money is that is being deposit or credit in account
-    string Name, Account;
+    float Amount, Money=0;            //Amount is total amount, Money is that is being deposit or credit in account
+    string Name, AccountNum;
     string password;
 
-public: 
+public:
     float Deposit(){
         cout << "Enter the amount: ";
         cin >> Money;
-        cout << Money << "Amount has been credited to your account";
+        cout << Money << "Amount has been credited to your account! \n";
         Amount = Amount + Money;
         return 0;
     }
     float Withdraw(){
-        cout<<"How much amount want to withdraw";
+        cout<<"How much amount want to withdraw \n ";
         cin >> Money;
         Amount = Amount - Money;
         return 0;
@@ -26,20 +26,26 @@ public:
         cout << "You have " <<Amount <<"Balance! \n";
         return 0;
     }
-        void CreateAccount(){
-            cout << "Enter your Whole Name";
-            getline(cin, Name);
-            cout <<"Enter Account Number";
-            getline(cin, Account);
-            cout << "Create a 5 Digit password";
-            cin >> password;
-            Amount = 0; 
-            cout <<"Your Account Created Successfully!";
+    void CreateAccount(){
+        cout << "Enter your Whole Name";
+        getline(cin, Name);
+        cout <<"Enter Account Number";
+        getline(cin, AccountNum);
+        cout << "Create a 5 Digit password";
+        cin >> password;
+        Amount = 0; 
+        cout <<"Your Account Created Successfully!";
     }
-
 };
 
-int main(){
+int main()
+{
+    Bank Account;
+    Account.Deposit();
+    Account.Withdraw();
+    Account.CheckBalance();
+    Account.CreateAccount();
 
 }
+
 
